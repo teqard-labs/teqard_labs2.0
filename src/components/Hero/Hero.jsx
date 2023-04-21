65ebf08990b84cb09e57cd37663e890263249dd4
Sid9021 <sidharthkajith9021@gmail.com>
2023-04-21 12:16:22 +0530

    added carousel

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import styles from "../../style";
 import { discount, robot } from "../../assets";
 import GetStarted from "./GetStarted";
+import Carousel from 'nuka-carousel';
 
 const Hero = () => {
   return (
@@ -42,11 +43,20 @@ const Hero = () => {
       <div
         className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
       >
-        <img
-          src={robot}
-          alt="billing"
+        <Carousel
+          wrapAround={true}
+          autoplay={true}
+          autoplayInterval={3000}
+          renderCenterLeftControls={({ previousSlide }) => null}
+          renderCenterRightControls={({ nextSlide }) => null}
+          renderBottomCenterControls={({ currentSlide }) => null}
           className="w-[100%] h-[100%] relative z-[5]"
-        />
+        >
+          <img src={robot} alt="Image 1" />
+          <img src={robot} alt="Image 2" />
+          <img src={robot} alt="Image 3" />
+          {/* Add more slides as desired */}
+        </Carousel>
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
