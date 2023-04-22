f1800e4f3a96576c30e3c4797fbbe8581206ebb9
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2023-04-22 19:57:58 +0530

    theme changed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
+        primary: "#ffffff",
+        secondary: "#00276b",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
       },
