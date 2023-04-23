986a9536c0c99074e7fc4a7d22b4da47f35daa2c
SS_11_RED <sssavio11@gmail.com>
2023-04-23 13:38:57 +0530

    added contact us page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,52 +10,38 @@ const Footer = () => {
   const iconBright = theme === 'light' ? 'brightness-0' : '';
   return(
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart}  md:flex-row flex-col mb-8 w-full`}>
-      <div className="flex-[1] flex flex-col justify-start mr-10">
-        <img
-          src={logo}
-          alt="hoobank"
-          className="w-[266px] h-[72.14px] object-contain"
-        />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make the payments easy, reliable and secure.
-        </p>
-      </div>
-
-      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-        {footerLinks.map((footerlink) => (
-          <div
-            key={footerlink.title}
-            className={`flex flex-col ss:my-0 my-4 min-w-[150px] `}
-          >
-            <h4 className={`font-poppins font-medium text-[18px] leading-[27px]${textColor}`}>
-              {footerlink.title}
-            </h4>
-            <ul className="list-none mt-4">
-              {footerlink.links.map((link, index) => (
-                <li
-                  key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] ${textColor} hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
-                >
-                  {link.name}
-                </li>
-              ))}
-            </ul>
+    <div classname="w-1/4">
+        {/* component */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <style dangerouslySetInnerHTML={{__html: "\n  .icon::after{\n  content: '';\n  display: block;\n  position: absolute;\n  border-top: 23px solid transparent;\n  border-bottom: 17px solid transparent;\n  border-left: 12px solid #3182ce;\n  left: 100%;\n  top: 0;\n}\n  " }} />
+        <form action className="form bg-white p-40 my-10 relative ">
+          <div className="icon bg-blue-gradient text-white w-6 h-6 absolute flex items-center justify-center p-5" style={{left: '-40px'}}><i className="fal fa-phone-volume fa-fw text-2xl transform -rotate-45" /></div>
+          <h3 className="text-4xl text-gray-900 font-semibold pb-10">Let us call you!</h3>
+          
+          <div className="flex space-x-5 mt-3">
+            <input type="text" name id placeholder="Your Name" className="border p-2  w-1/2" />
+            <input type="tel" name id placeholder="Your Number" className="border p-2 w-1/2" />
           </div>
-        ))}
+          <input type="email" name id placeholder="Your Email" className="border p-2 w-full mt-3" />
+          <textarea name id cols={10} rows={3} placeholder="Tell us about desired property" className="border p-2 mt-3 w-full" defaultValue={""} />
+          <p className="font-bold text-sm mt-3">GDPR Agreement *</p>
+          <div className="flex items-baseline space-x-2 mt-2">
+            <input type="checkbox" name id className="inline-block" />
+            <p className="text-gray-600 text-sm">I consent to having this website store my submitted information so they can respond to my inquiry.</p>
+          </div>
+          <input type="submit" defaultValue="Submit" className="w-full mt-6 bg-blue-gradient hover:bg-black text-white font-semibold p-3" />
+        </form>
       </div>
-    </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className={`font-poppins font-normal text-center text-[18px] leading-[27px]${textColor}`}>
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+        Copyright Ⓒ 2022 TeqardLabs. All Rights Reserved.
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
-  {socialMedia.map((social, index) => (
-    <img
+    {socialMedia.map((social, index) => (
+     <img
       key={social.id}
       src={social.icon}
       alt={social.id}
@@ -63,9 +49,9 @@ const Footer = () => {
         index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
       } filter ${iconBright} ${iconColor}`}
       onClick={() => window.open(social.link)}
-    />
-  ))}
-</div>
+     />
+     ))}
+    </div>
 
     </div>
   </section>
