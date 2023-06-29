9c4cd459bf34458e1e65c58b07b462b9af88766b
mridulkrishna24 <mridulkrishna24@gmail.com>
2023-06-29 15:11:35 +0530

    images n contents

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,50 +1,32 @@
-import { apple, bill, google } from "../assets";
+import tech2Image from "../assets/tech2.jpg";
 import styles, { layout } from "../style";
-import { useTheme } from "./ThemeContext"
+import { useTheme } from "./ThemeContext";
 
 const Billing = () => {
-
   const { theme } = useTheme();
-  const textColor = theme === 'light' ? 'text-black' : 'text-white';
-  return(
-  <section id="product" className={layout.sectionReverse}>
-    <div className={layout.sectionImgReverse}>
-      <img
-        src={bill}
-        alt="billing"
-        className="w-[100%] h-[100%] relative z-[5]"
-      />
+  const textColor = theme === "light" ? "text-black" : "text-white";
 
-      {/* gradient start */}
-      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full blue__gradient" />
-      {/* gradient end */}
-    </div>
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={tech2Image} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-    <div className={`${layout.sectionInfo} ${textColor}`}>
-      <h2 className={styles.heading2}>
-       We build <br className="sm:block hidden" /> greater futures
-      </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-      Teqard labs is an IT services and business solutions 
-      organization that has been partnering with many business organizations
-       in their transformation journeys.
-      </p>
-
-      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img
-          src={apple}
-          alt="google_play"
-          className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer"
-        />
-        <img
-          src={google}
-          alt="google_play"
-          className="w-[144.17px] h-[43.08px] object-contain cursor-pointer"
-        />
+        {/* gradient start */}
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full blue__gradient" />
+        {/* gradient end */}
       </div>
-    </div>
-  </section>
-)};
+
+      <div className={`${layout.sectionInfo} ${textColor}`}>
+        <h2 className={styles.heading2}>
+          We build <br className="sm:block hidden" /> greater futures
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          We provide IT services and business solutions organization that has been partnering with many business organizations in their transformation journeys.
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Billing;
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,29 +1,27 @@
-import { card } from "../assets";
 import styles, { layout } from "../style";
 import Button from "./Button/Button";
-import { useTheme } from "./ThemeContext"
+import { useTheme } from "./ThemeContext";
+import techImage from "../assets/tech.jpg";
 
 const CardDeal = () => {
   const { theme } = useTheme();
   const textColor = theme === 'light' ? 'text-black' : 'text-white';
-  return(
-  <section className={layout.section}>
-    <div className={`${layout.sectionInfo} ${textColor}`}>
-      <h2 className={styles.heading2}>
-      We transforms businesses <br className="sm:block hidden" /> through technology.
-      </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-      Our expertise lies in guiding businesses through digital transformation and achieving tangible growth, leveraging 
-      our extensive experience and knowledge of the relevant context.
-      </p>
-
-      <Button styles={`mt-10`} />
-    </div>
-
-    <div className={layout.sectionImg}>
-      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
-    </div>
-  </section>
-)};
+  return (
+    <section className={layout.section}>
+      <div className={`${layout.sectionInfo} ${textColor}`}>
+        <h2 className={styles.heading2}>
+        Driving Digital Transformation for Tangible Growth.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our expertise specializes in delivering innovative solutions that optimize operational efficiency and empower organizations to adapt to the ever-evolving digital landscape, all backed by our deep industry expertise and contextual understanding.
+        </p>
+        <Button styles={`mt-10`} />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={techImage} alt="Technology" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
+};
 
 export default CardDeal;
