1ab9cfc2f04c26b96124ac5654b51e4b150c1267
Sid9021 <sidharthkajith9021@gmail.com>
2023-10-05 15:52:43 +0530

    added email integration

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles, { layout } from "../style";
 import { useTheme } from "./ThemeContext";
-import techImage from "../assets/tech.jpg";
 
 const CardDeal = () => {
   const { theme } = useTheme();
